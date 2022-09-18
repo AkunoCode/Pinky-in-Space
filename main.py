b468acc9b9dc5886e1e85feafd28775ce7cc1ae4
John Leo Echevaria <echevariajohnleo@gmail.com>
2022-09-18 15:38:23 +0800

    Added Enemy Movement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 pygame.init()
@@ -27,10 +28,11 @@ enemyIMG = pygame.image.load('Images\monster_Enemy.png')
 enemyIMG = pygame.transform.scale(enemyIMG, (100,75)) # Resize the image
 
 # Enemy default coordinates related to screen size (x = 800, y = 600)
-enemyX = 350
-enemyY = 50
+enemyX = random.randint(0,700)
+enemyY = random.randint(50,150)
 # Change in coordinate
-enemyX_change = 0
+enemyX_change = 0.3
+enemyY_change = 15
 
 def player(x,y): # takes x and y as parameter to change position
     # Screen.blit() draws the image of the enemy.
@@ -54,9 +56,9 @@ while running:
         # To check if keystroke is pressed and whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                playerX_change = - 0.2
+                playerX_change = - 0.3
             if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                playerX_change = 0.2
+                playerX_change = 0.3
         # To check if keystroke is released
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_a or event.key == pygame.K_RIGHT or event.key == pygame.K_d:
@@ -65,11 +67,21 @@ while running:
     # Move by change
     playerX += playerX_change
 
-    # Boundaries
+    # Boundaries for player
     if playerX <= 0:
         playerX = 0
     elif playerX >= 675: # 675 because the player image is 125width (800 - 125 = 675)
         playerX = 675
+
+    enemyX += enemyX_change
+
+    # Boundaries for enemy
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 700: # 675 because the player image is 125width (800 - 125 = 675)
+        enemyX_change = -0.3
+        enemyY += enemyY_change
 
     # Call the player
     player(playerX, playerY)
